9f3ac831fcdd398738f398f7abbd99f7edf94e71
Davide Barasti <dbarasti@gmail.com>
2020-01-15 00:54:35 +0100

    exercise 1 ended

--- ListBag.hs
+++ ListBag.hs
@@ -1,19 +1,69 @@
-import Data.List
-data ListBag a = ListBag [(a, Int)]
+module ListBag where
+
+import Data.Maybe
+
+data ListBag a = ListBag [(a, Int)] 
   deriving (Show, Eq)
 
-tailLB (ListBag x) = tail x
-headLB (ListBag x) = head x
-getLB (ListBag x) = x
-search:: Eq a => a -> [(a,b)] -> Bool
-search a ((x,y):rest) = if x == a
-                          then True
-                          else search a rest
-search _ [] = False 
+empty = ListBag []
+
+singleton v = ListBag [(v,1)]
+
+fromList :: Eq a => [a] -> ListBag a
+fromList [x] = add x empty
+fromList (x:xs) = add x (fromList xs)
+
+search :: Eq a => a -> [(a,Int)] -> Maybe (a, Int)
+search _ [] = Nothing 
+search a ((x,y):rest) | a == x = Just (x,y)
+                      | otherwise = search a rest
+
+searchLB :: Eq a => a -> ListBag a -> Maybe (a, Int)
+searchLB a (ListBag x) = search a x 
+  
+
+push_front :: (a, Int) -> ListBag a -> ListBag a 
+push_front (x,y) (ListBag z)= ListBag ([(x,y)] ++ z)
+
+add :: Eq a => a -> ListBag a -> ListBag a
+add a (ListBag ((x, y):rest))
+      | a == x =  ListBag ((x, y + 1):rest)
+      | otherwise = push_front (x, y) (add a (ListBag rest))
+add a (ListBag []) = singleton a
+
 
 wf :: Eq a => ListBag a -> Bool
-wf (ListBag ((x,y):rest)) = if (search x rest) == False
-                  then   
-                    wf(ListBag rest)
-                  else False
+wf (ListBag ((x,y):rest)) 
+      | (search x rest) == Nothing = wf(ListBag rest)
+      | otherwise = False
 wf (ListBag []) = True
+
+isEmpty :: ListBag a -> Bool
+isEmpty (ListBag []) = True
+isEmpty (ListBag _) = False
+
+mul :: Eq a => a -> ListBag a -> Int
+mul v bag
+      | result == Nothing = 0
+      | otherwise = snd (fromJust result)
+      where result = searchLB v bag
+
+
+listify :: (a, Int) -> [a]
+listify (x,y) 
+      | y == 0 = []
+      | otherwise = x : listify (x, y-1)
+
+toList :: ListBag a -> [a]
+toList (ListBag ((x,y):rest)) = (listify (x,y)) ++ (toList (ListBag rest))
+toList (ListBag []) = []  
+
+addTuple :: Eq a => (a, Int) -> ListBag a -> ListBag a 
+addTuple t (ListBag ((x,y):rest))
+      | (fst t) == x = ListBag((x,y + (snd t)):rest)
+      | otherwise = push_front (x, y) (addTuple t (ListBag rest))
+addTuple t (ListBag []) = ListBag [t]      
+
+sumBag :: Eq a => ListBag a -> ListBag a -> ListBag a 
+sumBag bag (ListBag [y]) =  addTuple y bag
+sumBag bag (ListBag (y:ys)) = sumBag (addTuple y bag) (ListBag ys)
---
+++ test.hs
@@ -0,0 +1,4 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-13.7 script
+import ListBag
+lb = [1 .. 20]
